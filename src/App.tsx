96e2d61532cb1db5417e9debc991c6a63c830ac6
kreindo <ahmadsan4p@gmail.com>
2021-09-01 22:41:00 +0800

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import './App.css';
 // import Cart from 'scenes/Cart';
 
 function App() {
-  const [dat, setDat] = React.useState({});
+  const [dat, setDat] = React.useState(true);
+  const [post, setPosts] = React.useState();
+  const [post2, setPosts2] = React.useState();
 
   React.useEffect(() => {
     getData();
@@ -20,15 +22,35 @@ function App() {
         return response.json();
       })
       .then((response: any) => {
-        console.log(response);
+        setDat(false);
+        console.log(response.quote);
+        if (response.quote) {
+          setPosts(response.quote);
+          setPosts2(response.character);
+        } else {
+          console.log('An error happened when fetching data');
+        }
       })
       .catch((error) => {
+        setDat(false);
         console.log('an error happened.. ' + error);
       });
   };
+
+  const content = dat ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="center">
+      <pre>{JSON.stringify(post2, null, 1)}</pre>
+      <br />
+      <pre>{JSON.stringify(post, null, 1)}</pre>
+    </div>
+  );
+
   return (
     <div className="App">
-      <div>hello test</div>
+      <div>Quotes</div>
+      {content}
       {/* <Login></Login>
       <p>hello</p>
       <p>{dat.title}</p>
