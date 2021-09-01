7ff53fcf48dbeae4014af55baac98b79c93d9929
kreindo <ahmadsan4p@gmail.com>
2021-09-01 15:09:12 +0800

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import CC from 'component/ShopCard';
-import React, { useState, useEffect } from 'react';
-import Login from 'scenes/Login';
+// import CC from 'component/ShopCard';
+// import Login from 'scenes/Login';
+import React, { useState } from 'react';
 import './App.css';
 // import Homepage from 'scenes/Homepage';
 // import Cart from 'scenes/Cart';
 
 function App() {
-  const [dat, setDat] = useState(null);
+  const [dat, setDat] = React.useState({});
 
-  useEffect(() => {
+  React.useEffect(() => {
     getData();
-
-    async function getData() {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts/1'
-      );
-      const data = await response.json();
-      const final = data.quotes;
-      setDat(final);
-    }
-
-    return () => {};
   }, []);
 
+  const getData = () => {
+    const _FIRST__URL = 'https://jsonplaceholder.typicode.com/posts/1';
+    const _SECOND__URL = 'https://animechan.vercel.app/api/random';
+    fetch(_SECOND__URL)
+      .then((response: any) => {
+        return response.json();
+      })
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('an error happened.. ' + error);
+      });
+  };
   return (
     <div className="App">
-      {dat && <div>{dat}</div>}
-
+      <div>hello test</div>
       {/* <Login></Login>
       <p>hello</p>
       <p>{dat.title}</p>
@@ -34,5 +36,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
